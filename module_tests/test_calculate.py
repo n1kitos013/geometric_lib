2cde15b4a3d3e14b390bad5d340b2dba66d9ed8d
n1kitos013 <nikita16041922@gmail.com>
2024-12-18 00:28:35 +0300

    done 12

--- module_tests/test_calculate.py
+++ module_tests/test_calculate.py
@@ -1,185 +1,66 @@
-import sys
 import unittest
-
+import sys
 sys.path.append("/Users/User/geometric_lib")
 import calculate
 
+class TestCalculate(unittest.TestCase):
 
-class TestSquareArea(unittest.TestCase):
+    def test_circle_perimeter(self):
+        result = calculate.calc('circle', 'perimeter', [5])
+        self.assertEqual(result, 31.41592653589793)  
 
-    def test_first_area(self):
-        fig = "square"
-        func = "area"
-        size = [10]
-        result = calculate.calc(fig, func, size)
-        expected_result = 100
+    def test_circle_area(self):
+        result = calculate.calc('circle', 'area', [5])
+        self.assertAlmostEqual(result, 78.53981633974483) 
 
-        self.assertEqual(result, expected_result)
+    def test_square_perimeter(self):
+        result = calculate.calc('square', 'perimeter', [5])
+        self.assertEqual(result, 20)
 
-    def test_second_area(self):
-        fig = "square"
-        func = "area"
-        size = [-10]
-        result = calculate.calc(fig, func, size)
-        expected_result = 404
+    def test_square_area(self):
+        result = calculate.calc('square', 'area', [5])
+        self.assertEqual(result, 25)
 
-        self.assertEqual(result, expected_result)
+    def test_triangle_perimeter(self):
+        result = calculate.calc('triangle', 'perimeter', [3, 4, 5])
+        self.assertEqual(result, 12)
 
-    def test_third_area(self):
-        fig = "square"
-        func = "area"
-        size = [10**9]
-        result = calculate.calc(fig, func, size)
-        expected_result = 10**18
+    def test_triangle_area(self):
+        result = calculate.calc('triangle', 'area', [3, 4, 5])
+        self.assertAlmostEqual(result, 6.0)  
 
-        self.assertEqual(result, expected_result)
+    def test_triangle_invalid_sides(self):
+        result = calculate.calc('triangle', 'perimeter', [1, 2, 5])
+        self.assertEqual(result, "Can not make a triangle out of these elements")
 
+    def test_invalid_input_positive(self):
+        result = calculate.calc('circle', 'perimeter', [-5])
+        self.assertEqual(result, "All elements must be positive!")
 
-class TestSquarePerimeter(unittest.TestCase):
+    def test_circle_perimeter_zero(self):
+        result = calculate.calc('circle', 'perimeter', [0])
+        self.assertEqual(result, "All elements must be positive!")
 
-    def test_first_perimeter(self):
-        fig = "square"
-        func = "perimeter"
-        size = [10]
-        result = calculate.calc(fig, func, size)
-        expected_result = 40
+    def test_square_area_zero(self):
+        result = calculate.calc('square', 'area', [0])
+        self.assertEqual(result, "All elements must be positive!")
 
-        self.assertEqual(result, expected_result)
+    def test_triangle_perimeter_zero(self):
+        result = calculate.calc('triangle', 'perimeter', [0, 0, 0])
+        self.assertEqual(result, "All elements must be positive!")
 
-    def test_second_perimeter(self):
-        fig = "square"
-        func = "perimeter"
-        size = [-10]
-        result = calculate.calc(fig, func, size)
-        expected_result = 404
+    def test_triangle_area_zero(self):
+        result = calculate.calc('triangle', 'area', [3, 4, 5])
+        self.assertAlmostEqual(result, 6.0)
 
-        self.assertEqual(result, expected_result)
+    def test_triangle_area_zero_sides(self):
+        result = calculate.calc('triangle', 'area', [0, 0, 0])
+        self.assertEqual(result, "All elements must be positive!")
 
-    def test_third_perimeter(self):
-        fig = "square"
-        func = "perimeter"
-        size = [10**9]
-        result = calculate.calc(fig, func, size)
-        expected_result = 10**9 * 4
+    def test_triangle_perimeter_large_sides(self):
+        result = calculate.calc('triangle', 'perimeter', [100, 200, 300])
+        self.assertEqual(result,"Can not make a triangle out of these elements")
 
-        self.assertEqual(result, expected_result)
-
-
-class TestTriangleArea(unittest.TestCase):
-
-    def test_first_area(self):
-        fig = "triangle"
-        func = "area"
-        size = [10, 10, 10]
-        result = calculate.calc(fig, func, size)
-        expected_result = 43.30127018922193
-
-        self.assertEqual(result, expected_result)
-
-    def test_second_area(self):
-        fig = "triangle"
-        func = "area"
-        size = [-10, 10, 10]
-        result = calculate.calc(fig, func, size)
-        expected_result = 404
-
-        self.assertEqual(result, expected_result)
-
-    def test_third_area(self):
-        fig = "triangle"
-        func = "area"
-        size = [10, 20, 30]
-        result = calculate.calc(fig, func, size)
-        expected_result = 404
-
-        self.assertEqual(result, expected_result)
-
-
-class TestTrianglePerimeter(unittest.TestCase):
-
-    def test_first_perimeter(self):
-        fig = "triangle"
-        func = "perimeter"
-        size = [10, 10, 10]
-        result = calculate.calc(fig, func, size)
-        expected_result = 30
-
-        self.assertEqual(result, expected_result)
-
-    def test_second_perimeter(self):
-        fig = "triangle"
-        func = "perimeter"
-        size = [-10, 10, 10]
-        result = calculate.calc(fig, func, size)
-        expected_result = 404
-
-        self.assertEqual(result, expected_result)
-
-    def test_third_perimeter(self):
-        fig = "triangle"
-        func = "perimeter"
-        size = [10, 20, 30]
-        result = calculate.calc(fig, func, size)
-        expected_result = 404
-
-        self.assertEqual(result, expected_result)
-
-
-class TestCircleArea(unittest.TestCase):
-
-    def test_first_area(self):
-        fig = "circle"
-        func = "area"
-        size = [10]
-        result = calculate.calc(fig, func, size)
-        expected_result = 314.1592653589793
-
-        self.assertEqual(result, expected_result)
-
-    def test_second_area(self):
-        fig = "circle"
-        func = "area"
-        size = [-10]
-        result = calculate.calc(fig, func, size)
-        expected_result = 404
-
-        self.assertEqual(result, expected_result)
-
-    def test_third_area(self):
-        fig = "circle"
-        func = "area"
-        size = [10**9]
-        result = calculate.calc(fig, func, size)
-        expected_result = 3.1415926535897933e18
-
-        self.assertEqual(result, expected_result)
-
-
-class TestCirclePerimeter(unittest.TestCase):
-
-    def test_first_perimeter(self):
-        fig = "circle"
-        func = "perimeter"
-        size = [10]
-        result = calculate.calc(fig, func, size)
-        expected_result = 62.83185307179586
-
-        self.assertEqual(result, expected_result)
-
-    def test_second_perimeter(self):
-        fig = "circle"
-        func = "perimeter"
-        size = [-10]
-        result = calculate.calc(fig, func, size)
-        expected_result = 404
-
-        self.assertEqual(result, expected_result)
-
-    def test_third_perimeter(self):
-        fig = "circle"
-        func = "perimeter"
-        size = [10**9]
-        result = calculate.calc(fig, func, size)
-        expected_result = 6283185307.179586
-
-        self.assertEqual(result, expected_result)
+    def test_triangle_area_large_sides(self):
+        result = calculate.calc('triangle', 'area', [10, 10, 10])
+        self.assertAlmostEqual(result, 43.30127018922193)
